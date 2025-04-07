a557d125fe75d62dd57ba7abf5706e66d47a85ee
achraf <achrafsikal@gmail.com>
2025-04-07 01:06:20 +0100

    feat: add dates, DB and models configurations to AppServiceProvider for maintainability and consistency

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureDates();
+    }
+    /**
+     * Configure the app commands
+     * @return void
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        ); // prohibit user from runnig DB destructive commands in production like "migrate:fresh"
+    }
+    /**
+     * Configure the app models
+     * @return void
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
+
+        Model::shouldBeStrict();
+    }
+    /**
+     * Configure the app Dates
+     * @return void
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+        // Make date objects immutable to prevent wierd behavior whene minepulating dates
     }
 }
