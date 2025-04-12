3b80d34341e08d11105604985fc489602c440446
achraf <achrafsikal@gmail.com>
2025-04-12 18:24:12 +0100

    refactor: remove rememberTocken column from user table and UserFactory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin','creator','user'])->default('user');
-            $table->rememberToken();
             $table->timestamps();
         });
 
