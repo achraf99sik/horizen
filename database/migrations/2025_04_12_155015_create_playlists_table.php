bbef6cdd992bb1f84cc3200139e76a49271470bf
achraf <achrafsikal@gmail.com>
2025-04-12 17:17:46 +0100

    Database: adjust playlists table schema

--- database/migrations/2025_04_12_155015_create_playlists_table.php
+++ database/migrations/2025_04_12_155015_create_playlists_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('playlists', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
