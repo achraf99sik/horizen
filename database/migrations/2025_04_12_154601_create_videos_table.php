dbff07ef03831478c6263abd814d9a2c22695741
achraf <achrafsikal@gmail.com>
2025-04-12 18:00:56 +0100

    fix: puting tables in the write order and creating nationalities table

--- database/migrations/2025_04_12_154601_create_videos_table.php
+++ database/migrations/2025_04_12_154601_create_videos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->longText('subtitle');
             $table->longText('media');
-            $table->longText('thumbnail');
+            $table->binary('thumbnail');
             $table->longText('description');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
