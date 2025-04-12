7bf517e1b2c67fbbf9e8574498042fe400cae2a3
achraf <achrafsikal@gmail.com>
2025-04-12 17:20:39 +0100

    Database: adjust watch_histories table schema

--- database/migrations/2025_04_12_155157_create_watch_histories_table.php
+++ database/migrations/2025_04_12_155157_create_watch_histories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('watch_histories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('video_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
