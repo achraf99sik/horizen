6c747b96f602a3ee119d8e831d42d8d80f1a40e3
achraf <achrafsikal@gmail.com>
2025-04-12 17:18:59 +0100

    Database: adjust playlist_videos table schema

--- database/migrations/2025_04_12_155034_create_playlist_videos_table.php
+++ database/migrations/2025_04_12_155034_create_playlist_videos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('playlist_videos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('playlist_id')->constrained()->onDelete('cascade');
+            $table->foreignId('video_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
