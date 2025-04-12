1c2293c26dd85acfdd20509fdce4a6d2cdfe9b9e
achraf <achrafsikal@gmail.com>
2025-04-12 17:15:23 +0100

    Database: adjust videos table schema

--- database/migrations/2025_04_12_154953_create_videos_table.php
+++ database/migrations/2025_04_12_154953_create_videos_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('subtitle');
+            $table->longText('media');
+            $table->longText('thumbnail');
+            $table->longText('description');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
