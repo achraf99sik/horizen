3bf3a4fed9d7ba8fe1090741bf1b65ceecb8eba8
achraf <achrafsikal@gmail.com>
2025-04-13 00:48:58 +0100

    feat: adjusting playlist model relations

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -6,9 +6,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 final class Playlist extends Model
 {
     /** @use HasFactory<\Database\Factories\PlaylistFactory> */
     use HasFactory;
+
+    /**
+     * Get the user who made this playlist
+     * @return BelongsTo<User, playlist>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class);
+    }
 }
