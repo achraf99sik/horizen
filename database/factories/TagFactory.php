7fb9afea4849a9c2ac096c1a7712532cfb9086df
achraf <achrafsikal@gmail.com>
2025-04-12 19:03:57 +0100

    feat: adjusting TagFactory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class TagFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "name"=> $this->faker->name,
+            "video_id" => Video::factory(),
         ];
     }
 }
