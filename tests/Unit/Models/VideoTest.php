0c40196ca12982e58a8a4100196ec71830717e90
achraf <achrafsikal@gmail.com>
2025-04-13 17:46:32 +0100

    test: implementing video model test

---
+++ tests/Unit/Models/VideoTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Video;
+
+test('Video to array', function () {
+    $video = Video::factory()->create()->refresh();
+
+    expect(array_keys($video->toArray()))
+        ->toBe([
+            'id',
+            'title',
+            'subtitle',
+            'media',
+            'thumbnail',
+            'description',
+            'user_id',
+            'category_id',
+            'created_at',
+            'updated_at',
+        ]);
+});
+
+test('Video relationships', function () {
+    $video = Video::factory()->create();
+
+    expect($video->user)->not()->toBeNull();
+    expect($video->category)->not()->toBeNull();
+    expect($video->tags)->toBeInstanceOf(\Illuminate\Database\Eloquent\Collection::class);
+    expect($video->viewer)->toBeInstanceOf(\Illuminate\Database\Eloquent\Collection::class);
+});
