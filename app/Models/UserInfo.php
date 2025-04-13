ab2e8d9d7ad3e45d8b0f4ebbde44cb46a2256c8f
achraf <achrafsikal@gmail.com>
2025-04-13 01:12:36 +0100

    feat: adjusting userinfo model relations and creating nationality model

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -6,9 +6,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 final class UserInfo extends Model
 {
     /** @use HasFactory<\Database\Factories\UserInfoFactory> */
     use HasFactory;
+
+    /**
+     * Get user with user info
+     * @return HasOne<User, UserInfo>
+     */
+    public function user(): HasOne
+    {
+        return $this->HasOne(User::class);
+    }
+
+    /**
+     * Get nationality of a given user
+     * @return HasOne<Nationality, UserInfo>
+     */
+    public function nationality(): HasOne
+    {
+        return $this->HasOne(Nationality::class);
+    }
 }
