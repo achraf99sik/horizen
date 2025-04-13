cb3e3cea89fd29e51d419a5d3ac0cf8e2404ce06
achraf <achrafsikal@gmail.com>
2025-04-13 01:16:20 +0100

    feat: adjusting nationality model relations

--- app/Models/Nationality.php
+++ app/Models/Nationality.php
@@ -5,8 +5,16 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Nationality extends Model
 {
-    //
+    /**
+     * Get all the users with this nationality
+     * @return HasMany<UserInfo, Nationality>
+     */
+    public function user(): HasMany
+    {
+        return $this->hasMany(UserInfo::class);
+    }
 }
