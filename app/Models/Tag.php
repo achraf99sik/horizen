4b0f740b68583cc4fa87b909653cebcd25f69470
achraf <achrafsikal@gmail.com>
2025-04-13 01:02:37 +0100

    feat: adjusting tag model relations

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,9 +6,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 final class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    public function video(): BelongsToMany
+    {
+        return $this->belongsToMany(Video::class);
+    }
 }
