2c35f4cd36cdf7caf75047129ecf9c80644a360a
achraf <achrafsikal@gmail.com>
2025-04-13 12:44:55 +0100

    feat: create nationality factory

--- app/Models/Nationality.php
+++ app/Models/Nationality.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Nationality extends Model
 {
+    use HasFactory;
     /**
      * Get all the users with this nationality
      * @return HasMany<UserInfo, Nationality>
