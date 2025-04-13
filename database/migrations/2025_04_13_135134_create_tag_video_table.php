37a8bb3ef108b2d807efac58c0a47556c4cebc12
achraf <achrafsikal@gmail.com>
2025-04-13 17:24:19 +0100

    refactor: remove dead code and unactive namespaces

--- database/migrations/2025_04_13_135134_create_tag_video_table.php
+++ database/migrations/2025_04_13_135134_create_tag_video_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('tag_video', function (Blueprint $table) {
+        Schema::create('tag_video', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->foreignId('video_id')->constrained()->onDelete('cascade');
