cc1dfed8e8b5707274512d4acb00c84d45092753
achraf <achrafsikal@gmail.com>
2025-04-17 18:35:42 +0100

    feat: adding vidstack css style to the layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
 import "./globals.css";
 
 const geistSans = Geist({
