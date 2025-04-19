2779ee23a0cbb74f81b288b4f3556074c8b497aa
achraf <achrafsikal@gmail.com>
2025-04-19 03:46:22 +0100

    test: test azure deployment

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/home', function () {
+    return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
 });
