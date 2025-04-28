9d308a728995558588ac396aabaaceceaab4d1a1
achraf <achrafsikal@gmail.com>
2025-04-28 14:46:07 +0100

    fix: removing unessacery controller class uses

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+
 use App\Models\UserInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
