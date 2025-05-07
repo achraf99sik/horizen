8ad00558dc21eb79f315f6e21cee465f8cfaf1c2
achraf <achrafsikal@gmail.com>
2025-05-07 12:03:48 +0100

    Security: adding csrf token to the category form

--- resources/views/admin/create-category.blade.php
+++ resources/views/admin/create-category.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
         <form action="{{ route('categories.store') }}" id="categoryForm">
+            @csrf
             <input type="text" name="name" id="categoryName">
             <button type="submit">create</button>
         </form>
